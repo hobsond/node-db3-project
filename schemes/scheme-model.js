b2b46e1bb283a32daa0571d4f89743d8c320e8c8
hobsond <60270270+hobsond@users.noreply.github.com>
2020-06-10 15:56:37 -0400

    added find steps and find id functions

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,10 +3,19 @@ const db = require('../data/database')
 function find(){
   return  db.select('schemes.scheme_name','steps.instructions')
     .from('schemes')
-    .innerJoin('steps','schemes.id','steps.scheme_id')
+    .leftOuterJoin('steps','schemes.id','=','steps.scheme_id')
     .orderBy('schemes.id')
-    .orderBy('steps.scheme_id')
-    .orderBy('steps.step_number')
 }
 
-module.exports = {find}
+function findById(id){
+    return db('schemes')
+
+            .where({id:id})
+}
+
+function findSteps(id){
+  return  db('steps')
+    .where({scheme_id:id})
+
+}
+module.exports = {find,findById,findSteps}
